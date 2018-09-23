dd7bd815a123bfc38b2879fa524486e54d06bee7
Seb Gertz <sebgertz@gmail.com>
2018-09-23 23:35:54 +0000

    all tests passing

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
 }
+
+function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      return {friends: state.friends.filter(friend => friend.id !== action.id)}
+    default:
+      return state;
+  }
+}
